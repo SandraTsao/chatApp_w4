064eabac52a37109756a83543fc0ba4d30de93c1
Sandra Tsao <sandratsao@Sandras-MBP.fanshawec.ca>
2020-01-28 16:30:50 -0500

    back end done for now

--- app.js
+++ app.js
@@ -24,9 +24,13 @@ io.attach(server);
 
 io.on('connection', function(socket) {
     console.log('user connected');
+    socket.emit('connect', {sID: `${socket.id}`, message: 'new connection'});
 
     // listen for a disconnect
     socket.on('disconnect', function() {
         console.log('a user disconnected');
+
+        message = `${socket.id} has left the chat!`;
+        io.emit('user_disconnect', message);
     })
 })
--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,3 +1,16 @@
 // imports always go first - if we're importing anything
 
 const socket = io();
+
+function setUserId(packet) {
+    debugger;
+    console.log(packet);
+}
+
+function runDisconnectMessage(packet) {
+    debugger;
+    console.log(packet);
+}
+
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('user_disconnect', runDisconnectMessage);
